6ca2cbecb240f19c48bdb49c84e277342685463e
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-04-21 11:27:16 +0200

    Console.logs weggehaald

--- src/assets/js/controllers/dashboardController.js
+++ src/assets/js/controllers/dashboardController.js
@@ -28,10 +28,6 @@ class DashboardController {
         $(".content").html("Failed to load the sidebar!");
     }
 
-    //$("#book1").attr("src", photoLink.src);
-
-    //document.getElementById("book1").setAttribute("src", photoLink)
-
     async loadBook() {
 
         try {
@@ -39,79 +35,49 @@ class DashboardController {
 
             let DASHBOARD = await this.dashboardRepository.getFeatured();
 
-            console.log(DASHBOARD);
-
-
-
             app.loadController(DASHBOARD);
 
             //Load 1st book
 
             $("#book1").attr("src", DASHBOARD[0].Image);
 
-            console.log(DASHBOARD[0])
-
             //Load 2nd book
 
             $("#book2").attr("src", DASHBOARD[1].Image);
-
-            console.log(DASHBOARD[1])
 
             //Load 3th book
 
             $("#book3").attr("src", DASHBOARD[2].Image);
 
-            console.log(DASHBOARD[2])
 
             //Load 4th book
 
             $("#book4").attr("src", DASHBOARD[3].Image);
 
-            console.log(DASHBOARD[3])
 
             //Load 5th book
 
             $("#book5").attr("src", DASHBOARD[4].Image);
 
-            console.log(DASHBOARD[4])
 
             //Load 6th book
 
             $("#book6").attr("src", DASHBOARD[5].Image);
 
-            console.log(DASHBOARD[5])
 
             //Load 7th book
 
             $("#book7").attr("src", DASHBOARD[6].Image);
 
-            console.log(DASHBOARD[6])
 
             //Load 8th book
 
             $("#book8").attr("src", DASHBOARD[7].Image);
 
-            console.log(DASHBOARD[7])
 
             //Load 9th book
 
             $("#book9").attr("src", DASHBOARD[8].Image);
-
-            console.log(DASHBOARD[8])
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //document.getElementById("book1").setAttribute("src", DASHBOARD[0])
 
 
         } catch (e) {
